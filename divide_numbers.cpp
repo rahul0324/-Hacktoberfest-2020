db4deb957f7fa0613c1e30ebf42eb21435fe7373
compiletimeterror <56031091+compiletimeterror@users.noreply.github.com>
2020-10-01 14:49:02 +0530

    Add files via upload

---
+++ divide_numbers.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include<bits/stdc++.h>
+using namespace std;
+
+int divideNumbers(int a, int b){
+    return a/b;
+}
+int main(){
+    int a, b;
+    cin>>a>>b;
+    cout<<divideNumbers(a, b)<<endl;
+    return 0;
+}
+
+
